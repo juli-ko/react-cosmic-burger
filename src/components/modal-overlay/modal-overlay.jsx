7d23fc04ea81f66762d0c061f4f4dcd040ccb444
Yulia kolomiets <yuliakolomiets09@gmail.com>
2025-01-26 18:52:44 +0300

    fix(Modal): fix click on overlay, add propTypes to Modal and ModalOverlay

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,7 +1,12 @@
 import styles from './modal-overlay.module.scss';
+import PropTypes from 'prop-types';
 
 const ModalOverlay = ({ onClose }) => {
 	return <div className={styles.overlay} onClick={onClose}></div>;
 };
 
 export default ModalOverlay;
+
+ModalOverlay.propTypes = {
+	onClose: PropTypes.func.isRequired,
+};
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import ModalOverlay from '../modal-overlay/modal-overlay';
 import styles from './modal.module.scss';
@@ -23,19 +24,23 @@ const Modal = ({ children, header, onClose }) => {
 	return ReactDOM.createPortal(
 		<>
 			<ModalOverlay onClose={onClose}></ModalOverlay>
-			<div className={styles.container}>
-				<div className={styles.modal}>
-					<div className={`${styles.header} mt-10 mr-10 ml-10`}>
-						<h2 className='text text_type_main-large'>{header}</h2>
-						<button className={styles.closeBtn} onClick={onClose}>
-							<CloseIcon type='primary' />
-						</button>
-					</div>
-					{children}
+			<div className={styles.modal}>
+				<div className={`${styles.header} mt-10 mr-10 ml-10`}>
+					<h2 className='text text_type_main-large'>{header}</h2>
+					<button className={styles.closeBtn} onClick={onClose}>
+						<CloseIcon type='primary' />
+					</button>
 				</div>
+				{children}
 			</div>
 		</>,
 		modalRoot
 	);
 };
 export default Modal;
+
+Modal.propTypes = {
+	children: PropTypes.element.isRequired,
+	header: PropTypes.string,
+	onClose: PropTypes.func.isRequired,
+};
