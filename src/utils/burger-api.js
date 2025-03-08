c5b670a3ad7d3062f17792862547d0a212249c27
Yulia kolomiets <yuliakolomiets09@gmail.com>
2025-03-08 20:25:09 +0300

    feat: add fetch functions for auth

--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -23,3 +23,84 @@ export const fetchOrder = (idsArr) => {
 		}),
 	});
 };
+
+export const fetchForgotPassword = (email) => {
+	return request('/password-reset', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ email }),
+	});
+};
+
+export const fetchResetPassword = (password, token) => {
+	return request('/password-reset/reset', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ password, token }),
+	});
+};
+
+export const fetchRegister = (email, password, name) => {
+	return request('/auth/register', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ email, password, name }),
+	});
+};
+
+export const fetchLogin = (email, password) => {
+	return request('/auth/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ email, password }),
+	});
+};
+
+export const fetchLogout = () => {
+	return request('/auth/logout', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ token: `${localStorage.getItem('refreshToken')}` }),
+	});
+};
+
+export const fetchRefreshToken = () => {
+	return request('/auth/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ token: `${localStorage.getItem('refreshToken')}` }),
+	});
+};
+
+export const fetchGetUser = () => {
+	return request('/auth/user', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+		},
+	});
+};
+
+export const fetchRefreshUser = (userData) => {
+	return request('/auth/user', {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+		},
+		body: JSON.stringify(userData),
+	});
+};
